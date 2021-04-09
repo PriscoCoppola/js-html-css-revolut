cb6b9f83d3ced0c5e5eabef80f4402ec8e79b2d2
PriscoCoppola <priscocoppola7@gmail.com>
2021-04-09 17:04:36 +0200

    add js and finish

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('js ok');
+/*************************
+* REVOLUT
+*************************/
+
+// Referenze
+var dropdown = $('.header-right .with-dropdown');
+
+dropdown.click(function() {
+
+    dropdown.children('.dropdown').not($(this).children('.dropdown')).hide();
+    $(this).children('.dropdown').toggle();
+})
